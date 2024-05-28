d26126a743aa020efb7009962e95bcea64fab366
Dutrak <fernandodutra100701@gmail.com>
2024-05-27 23:20:42 -0300

    feat:Header and Hero

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { MapPin, ShoppingCart } from "phosphor-react";
+import Logo from "../assets/Logo.svg";
+
+export function Header() {
+  return (
+    <header className="flex flex-row justify-between">
+      <img src={Logo} alt="Logo da Alura" />
+      <div className="flex flex-row gap-3 ">
+        <div className="flex gap-0.5 bg-purple-light rounded-lg items-center justify-center h-9 p-2">
+          <MapPin size={16} weight="fill" className="text-purple" />
+          <span className="text-sm leading-snug font-normal text-purple-dark">Piracaia, SP</span>
+        </div>
+        <div className="w-9 h-9 bg-yellow-light pmr-7 rounded-lg flex justify-center items-center">
+          <a className="cursor-pointer"><ShoppingCart size={22} weight="fill" className=" text-yellow-dark" /></a>
+        </div>
+      </div>
+    </header>
+  )
+}
